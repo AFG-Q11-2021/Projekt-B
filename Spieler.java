4c93a7565104a392a22fbc043c8ab2c04d6a20c1
Lasse Wittmer <lassewittmer@googlemail.com>
2021-04-30 12:20:57 +0200

    lost

--- Spieler.java
+++ Spieler.java
@@ -13,10 +13,6 @@ public class Spieler
         int px = 1;//Startpunkt des Spielers//
         int py = 1;
 
-        stift.setColor(farbe);
-        stift.fillRect(px, py, 20, 20);
-        stift.setColor(Color.BLACK);
-        stift.drawRect(px, py, 20, 20);
     }
     public void paint (Graphics stift)
     {
