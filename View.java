08acc6cdbd7b7032527939ced4b88f01aadb1aae
LaurensBirkenbach <laurens@birkenbachs.de>
2021-04-30 12:41:38 +0200

    kommentare view dazu

--- View.java
+++ View.java
@@ -19,36 +19,39 @@ public class View
     }
 
     public void keyPressed(KeyEvent e) {
+
+        //vorwärts
         if(e.getKeyCode() == KeyEvent.VK_W){ 
             fwd = true;
         }
-        
+        //rückwärts
         if(e.getKeyCode() == KeyEvent.VK_S){ 
             fwd = true;
         }
-        
+        //links
         if(e.getKeyCode() == KeyEvent.VK_A){ 
             fwd = true;
         }
-        
+        //rechts
         if(e.getKeyCode() == KeyEvent.VK_D){ 
             fwd = true;
         }
     }
 
     public void keyReleased(KeyEvent e) {
+        //vorwärts
         if(e.getKeyCode() == KeyEvent.VK_W){ 
             fwd = false;
         }
-        
+        //rückwärts
         if(e.getKeyCode() == KeyEvent.VK_S){ 
             fwd = false;
         }
-        
+        //links
         if(e.getKeyCode() == KeyEvent.VK_A){ 
             fwd = false;
         }
-        
+        //rechts
         if(e.getKeyCode() == KeyEvent.VK_D){ 
             fwd = false;
         }
