ae1f0147620acc3fa747b5c9430534e37ccac93d
fieldmarschall <julius.rommel@mlbx.de>
2021-05-02 10:09:47 +0200

    fixed conflicts

--- Spieler.java
+++ Spieler.java
@@ -5,27 +5,14 @@ import java.awt.Color;
 
 public class Spieler
 {
-    private int x; // Postion x (Koordinatensystem)//
-    private int y; // Postion y (Koordinatensystem)//
+    private int px; // Postion x (Koordinatensystem)//
+    private int py; // Postion y (Koordinatensystem)//
     private String username;
     private int leben;
     public Spieler(String usernameNeu, Graphics stift)
     {
         username= usernameNeu;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        int px = 1;
-        int py = 1;
-=======
-        int px = 1;//Startpunkt des Spielers//
-        int py = 1;
 
-
-
-        
->>>>>>> a0bcf831b97d90f83ae8ce214e7affb08ffe235d
-=======
         int px = 1;//Startpunkt des Spielers//
         int py = 1;
 
@@ -34,59 +21,55 @@ public class Spieler
         stift.setColor(Color.BLACK);
         stift.drawRect(px,py, 20, 20);
 
-        
->>>>>>> a0bcf831b97d90f83ae8ce214e7affb08ffe235d
     }
     public void paint (Graphics stift)
     {
-        
+
     }
+
     public void geradeausGehen()
     {
         py--;
     }
+
     public void rueckwertsGehen()
     {
         py++;
     }
+
     public void linksGehen()
     {
         px--;
     }
+
     public void rechtsGehen()
     {
         px++;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public Cell getPosition()
-=======
+
     public Karte getPosition()
->>>>>>> a0bcf831b97d90f83ae8ce214e7affb08ffe235d
-=======
-    public Karte getPosition()
->>>>>>> a0bcf831b97d90f83ae8ce214e7affb08ffe235d
+
     {//Aktualisiert die Position des Spielers//
-        
-=======
+
         int x = 1;//Startpunkt des Spielers//
         int y = 1;
         int leben = 5;
->>>>>>> 20ae19b908190b92fb1fffc5261aefc02e1ec02a
+
     }
-    
+
     public void geradeausGehen()
     {
-       if(y>0)
-       {
-          y--;
-       }
+        if(y>0)
+        {
+            y--;
+        }
     }
+
     public void rueckwertsGehen()
     {
         if(y<Karte.getSizeX()) //Karte ist eine Klasse, muss noch in ein Objekt umgewandelt werden//
         {
-           y++;
+            y++;
         }
     }
 
@@ -94,7 +77,7 @@ public class Spieler
     {
         if(x>0)
         {
-           x--;
+            x--;
         }
     }
 
