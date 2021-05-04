757e8ed900a036fadc2e6696e6dc7f4a0bf2da8f
Niklas Rösner <niklas@roesner-home.de>
2021-05-04 09:36:35 +0200

    aktualisiert

--- MapEditor.java
+++ MapEditor.java
@@ -3,35 +3,47 @@ import java.io.*;
 /**
  * Write a description of class MapEditor here.
  *
- * @author Niklas Rösner, Laurens Birkenbach (30.04.2021)
+ * @author Niklas Rösner, Laurens Birkenbach (04.05.2021)
  * @version 0.1
  */
 public class MapEditor
 {
- 
-    public void karteAktualisieren(Karte karte, String dateiName){
+
+    public static void karteLaden(Karte karte, String dateiName){
         FileReader fr = new FileReader(dateiName);
         BufferedReader br = new BufferedReader(fr);
-        
+
         for(int y = 0; y < karte.getSizeY();y++){
             try{
-                    String line = br.readLine();
-                    String[] lineArray = line.split(" ");
-                    for(int x = 0; x < lineArray.length;x++){
-                        if(x < karte.getSizeX()){
-                             karte.setKartenArray(x,y,Integer.parseInt(lineArray[x]));
-                        }
-                   
+                String line = br.readLine();
+                String[] lineArray = line.split(" ");
+                for(int x = 0; x < lineArray.length;x++){
+                    if(x < karte.getSizeX()){
+                        karte.setKartenArray(x,y,Integer.parseInt(lineArray[x]));
                     }
-                    
+
+                }
+
             } catch (Exception e){
                 //Klingelin
             }
-    
-            
+
         }
     }
-    
-    
-    
+
+    public static void karteSpeichern(Karte karte, String dateiName){
+        File file =new File(dateiName);
+        
+        try{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file)); 
+        //Writer für Text
+        
+        
+        }
+        catch(Exception e){
+        //Klingeling
+        }
+        
+    } 
+
 }
