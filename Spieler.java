748912c016a6666178d4c024f75312f949d2ef8a
Martin <martin@michael-lex.de>
2021-05-04 09:33:38 +0200

    Spieler akltuell 9:33

--- Spieler.java
+++ Spieler.java
@@ -5,33 +5,33 @@ import java.awt.Color;
 
 public class Spieler
 {
-    private int px; // Postion x (Koordinatensystem)//
-    private int py; // Postion y (Koordinatensystem)//
+    private int x; // Postion x (Koordinatensystem)//
+    private int y; // Postion y (Koordinatensystem)//
     private String username;
     private int leben;
-    private Color farbe;
+    
     public Spieler(String usernameNeu, Graphics stift)
     {
         username= usernameNeu;
-
-
         int px = 1;//Startpunkt des Spielers//
         int py = 1;//Startpunkt des Spielers//
         int leben = 5;
-
-
     }
-    
-
 
     public void linksGehen()
     {
-        x--;
+        if(x>0)
+        {
+            x--;
+        }
     }
 
     public void rechtsGehen()
     {
-        x++;
+        if(y<Karte.getSizeX()) //Karte ist eine Klasse, muss noch in ein Objekt umgewandelt werden//
+        {
+            x++;
+        }
     }
 
 
@@ -44,10 +44,10 @@ public class Spieler
     
     public void geradeausGehen()
     {
-       if(y>0)
-       {
+        if(y>0)
+        {
           y--;
-       }
+        }
     }
     public void rueckwertsGehen()
     {
@@ -56,7 +56,10 @@ public class Spieler
            y++;
         }
     }
-
-
+    
+    public void rotation()
+    {
+        
+    }
     }
  
