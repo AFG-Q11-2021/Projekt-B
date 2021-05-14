5f74cb77938d7781d2f9852e6502d703e4ded704
fieldmarschall <julius.rommel@mlbx.de>
2021-05-14 15:18:10 +0200

    made mulltiplayer work

--- Framee.java
+++ Framee.java
@@ -13,7 +13,7 @@ public class Framee extends JFrame implements ActionListener
     private JButton schliessen;
     private JButton auswahl;
     private JButton ende;
-    
+
     public Framee(String title)
     {
         super(title);
@@ -42,12 +42,12 @@ public class Framee extends JFrame implements ActionListener
                 fenster();
                 this.setVisible(false);
             }
-            if(e.getSource() == auswahl)
+            else if(e.getSource() == auswahl)
             {
                 auswahl();
                 this.setVisible(false);
             }
-            if(e.getSource() == ende)
+            else if(e.getSource() == ende)
             {
                 System.exit(0);
             }
@@ -57,13 +57,12 @@ public class Framee extends JFrame implements ActionListener
     public static void fenster()
     {
         Game gamee = new Game();
-
         Controller.Setgame(gamee);
-
     }
 
     public static void auswahl()
     {
         Multiplayer multiplayer = new Multiplayer("Multiplayer");
+        Controller.Setmultiplayer(multiplayer);
     }
 }
--- Game.java
+++ Game.java
@@ -15,8 +15,8 @@ public class Game extends Canvas
     private String title = "Game";
     private final int WIDTH = 1000;
     private final int HEIGHT = 1000;
-    
-     private Karte kartetest;//für den Darsteller umschreiben
+
+    private Karte kartetest;//für den Darsteller umschreiben
     private Graphics graph;
     private int csizeX;
     private int csizeY;
@@ -33,7 +33,7 @@ public class Game extends Canvas
         frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame1.setLocationRelativeTo(null);
         frame1.setVisible(true);
-        
+
         kartetest = new Karte();
         kartetest.setKartenArray(2, 2, 1);
         csizeX = (int)WIDTH/kartetest.getSizeX();
@@ -45,33 +45,29 @@ public class Game extends Canvas
         this.createBufferStrategy(2);
         BufferStrategy bs = this.getBufferStrategy();
         Graphics g = bs.getDrawGraphics();
-       // g.setColor(Color.GREEN);
-       // g.fillRect(0,0,WIDTH,HEIGHT);
-        
-        g.setColor(Color.BLACK);
-        
+
         //Karte malen
-         for(int x=0;x<kartetest.getSizeX();x++){
+        g.setColor(Color.BLACK);
+        for(int x=0;x<kartetest.getSizeX();x++){
             for(int y=0;y<kartetest.getSizeY();y++){
-                if(kartetest.getCoordinate(x, y)!=0)
-                {
+                if(kartetest.getCoordinate(x, y)!=0){
                     g.fillRect(x*csizeX,y*csizeY,csizeX,csizeY);
                 }
                 else{
-                     g.drawRect(x*csizeX,y*csizeY,csizeX,csizeY);
+                    g.drawRect(x*csizeX,y*csizeY,csizeX,csizeY);
                 }
             }
         }
-        
+
         //Spieler malen
-        Spieler s = Controller.spieler;
-        
+        Spieler s = Controller.Getspieler();
+
         g.setColor(Color.RED);
-        g.fillOval((int) (s.getX() *  csizeX),(int) s.getY()*csizeY,10,10);
-        
+        g.fillOval((int)s.getX()*csizeX,(int)s.getY()*csizeY, 10, 10);
+
         this.graphics = g;
         bs.show();
-        
+
     }
 
 }
