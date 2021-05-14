72accbf4f2fe6c89be31149f9701ea6468d8d77b
fieldmarschall <julius.rommel@mlbx.de>
2021-05-14 12:12:55 +0200

    Kartendarsteller und KartenVerwalter neu erstellt

---
+++ KartenVerwalter.java
@@ -0,0 +1,59 @@
+//Author: Samuel T , Julius R
+import java.io.*;
+public class KartenVerwalter
+{
+    Karte activeMap;
+    
+    public KartenVerwalter()
+    {
+        
+    }
+    
+    public void setActiveMap(int bI)
+    {
+        activeMap = this.getMapFromStorage(bI);
+    }
+    
+    public Karte getMap()
+    {
+        return activeMap;
+    }
+    
+    private Karte getMapFromStorage(int buildIndex)
+    {
+        Karte gesucht=new Karte();
+        String g = buildIndex+".txt";
+        //soll eine karte aus den Speicher Auslesen
+        this.karteLaden(gesucht,g);
+        return gesucht;
+    }
+    
+    private void karteLaden(Karte karte, String dateiName){
+        FileReader fr = new FileReader(dateiName);
+        BufferedReader br = new BufferedReader(fr);
+        
+        for(int y = 0; y < karte.getSizeY();y++){
+            try{
+                String line = br.readLine();
+                String[] lineArray = line.split(" ");
+                for(int x = 0; x < lineArray.length;x++){
+                    if(x < karte.getSizeX()){
+                        karte.setKartenArray(x,y,Integer.parseInt(lineArray[x]));
+                    }
+
+ 
+
+                }
+
+ 
+
+            } catch (Exception e){
+                //Klingelin
+            }
+
+ 
+
+        }
+    }
+    
+}
