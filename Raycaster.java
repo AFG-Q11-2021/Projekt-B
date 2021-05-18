2905b34e814cd2c5f2a295f9270936689c24f52a
fieldmarschall <julius.rommel@mlbx.de>
2021-05-18 10:11:35 +0200

    rotation aadjustated

--- Raycaster.java
+++ Raycaster.java
@@ -1,7 +1,7 @@
 //Author of the System: Julius R and Samuel T
 //only the Caster should be called from the outside
 //to tell the caster which map to use go to...
-import Java.util;
+import Java.util.*;
 public class Raycaster
 {
     public KartenVerwaĺter karte;
@@ -48,4 +48,5 @@ public class Raycaster
                 texture[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
             }
     }
+}
 
