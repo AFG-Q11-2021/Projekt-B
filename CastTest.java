f5a3b765f4190dd8a2b68aab7339ca411212e49b
LaurensBirkenbach <laurens@birkenbachs.de>
2021-05-27 18:05:08 +0200

    Boden hinzugefuegt (funktioniert noch nicht)

--- CastTest.java
+++ CastTest.java
@@ -26,11 +26,15 @@ public class CastTest {
         double planeX = 0.66;
         double planeY = 0;
 
+        
         double oldPlaneX = planeX;
         planeX = planeX * Math.cos(rot) - planeY * Math.sin(rot);
         planeY = oldPlaneX * Math.sin(rot) + planeY * Math.cos(rot);
 
         Game game = Controller.getGame();
+
+        int screenWidth = game.getWidth();
+        int screenHeight = game.getHeight();
 
         //drawSky
         float fov = 52.85f;
@@ -38,23 +42,78 @@ public class CastTest {
         for(int dx = 0; dx < game.getWidth()/stepSize;dx++){
             int x = dx * stepSize;
             double camX = (2 * x / ((double) game.getWidth())) - 1;
-            double rayDirX = dirX + planeX * camX;
-            double rayDirY = dirY + planeY * camX;
-            double rayAngle = (-rot + (camX*0.583));
-            
+
+            double rayAngle = (rot + (camX*0.583));
+
             int texX = (int) ((rayAngle/6.283) * 1000);
-            if(texX < 0){ texX += 1000;}
-            if(texX > 1000) {texX -= 1000;};
-            int xdraw = game.getWidth()-x;
-             g.drawImage(Controller.getTextureManager().getSkyTexture(0),xdraw-stepSize-1,0,xdraw+stepSize ,game.getHeight(),
-                    texX,200,texX+1,400,null);
-            
+            if(texX < 0) texX += 1000;
+            if(texX > 1000) texX -= 1000;
+
+            g.drawImage(Controller.getTextureManager().getSkyTexture(0),x-stepSize-1,0,x+stepSize ,game.getHeight(),
+                texX,100,texX+1,250,null);
+
         }
 
-        g.setColor(new Color(180, 70, 0));
-        g.fillRect(0, game.getHeight() / 2, game.getWidth(), game.getHeight() / 2);
+        // g.setColor(new Color(180, 70, 0));
+        // g.fillRect(0, game.getHeight() / 2, game.getWidth(), game.getHeight() / 2);
 
-        
+        //Floor Casting?
+        for(int iy = game.getHeight()/(2*stepSize); iy < game.getHeight()/stepSize;iy++){
+            // rayDir for leftmost ray (x = 0) and rightmost ray (x = w)
+            int y = iy*stepSize;
+            double rayDirX0 = dirX - planeX;
+            double rayDirY0 = dirY - planeY;
+            double rayDirX1 = dirX + planeX;
+            double rayDirY1 = dirY + planeY;
+
+            int p = y - game.getHeight()/2;
+            double posZ = 0.5 *game.getHeight();
+
+            double rowDistance = posZ/p;
+
+            // calculate the real world step vector we have to add for each x (parallel to camera plane)
+            // adding step by step avoids multiplications with a weight in the inner loop
+            double floorStepX = rowDistance * (rayDirX1 - rayDirX0) / screenWidth;
+            double floorStepY = rowDistance * (rayDirY1 - rayDirY0) / screenWidth;
+
+            // real world coordinates of the leftmost column. This will be updated as we step to the right.
+            double floorX = xPos + rowDistance * rayDirX0;
+            double floorY = yPos + rowDistance * rayDirY0;
+
+            for(int ix = 0; ix < screenWidth/stepSize; ++ix)
+            {
+                int x = ix*stepSize;
+                // the cell coord is simply got from the integer parts of floorX and floorY
+                
+                int cellX = (int)(floorX);
+                int cellY = (int)(floorY);
+
+                // get the texture coordinate from the fractional part
+                int tx = (int)(texRes * (floorX - cellX)) & (texRes - 1);
+                int ty = (int)(texRes * (floorY - cellY)) & (texRes - 1);
+
+                floorX += floorStepX;
+                floorY += floorStepY;
+
+                // choose texture and draw the pixel
+
+                // floor
+                //color = texture[floorTexture][texRes * ty + tx];
+                //color = (color >> 1) & 8355711; // make a bit darker
+               // buffer[y][x] = color;
+                int drawX = screenWidth-x;
+                 g.drawImage(Controller.getTextureManager().getDarkTexture(0),drawX,y,drawX+stepSize ,y+stepSize,
+                tx,ty,tx+1,ty+1,null);
+                
+
+                //ceiling (symmetrical, at screenHeight - y - 1 instead of y)
+                //color = texture[ceilingTexture][texRes * ty + tx];
+                // color = (color >> 1) & 8355711; // make a bit darker
+                // buffer[screenHeight - y - 1][x] = color;
+            }
+        }
+
+        //WallCasting
         for (int fx = 0; fx < game.getWidth()/stepSize; fx++) {
             int x = fx * stepSize;
             double camX = (2 * x / ((double) game.getWidth())) - 1;
@@ -147,7 +206,6 @@ public class CastTest {
                 g.drawImage(Controller.getTextureManager().getDarkTexture(texID),xdraw-stepSize-1,topPixel,xdraw+stepSize ,topPixel + columnHeight,
                     texX,0,texX+1,texRes,null);
             }
-
 
             // g.fillRect(xdraw, topPixel, 1, columnHeight);
         }
