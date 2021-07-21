0d4035e550064b0ac96ab17f9f10ebe67904e931
fieldmarschall <julius.rommel@mlbx.de>
2021-07-21 21:47:50 +0200

    ~

--- KeyHandler.java
+++ KeyHandler.java
@@ -5,187 +5,180 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 //Author: Julius
-public class KeyHandler extends MouseAdapter implements KeyListener {
-    private Controller con;
-    private Returner game;
-    private Spieler s;
+public class KeyHandler extends KeyAdapter {
+	private Controller con;
+	private Returner game;
+	private Spieler s;
 
-    private double speedm;
-    private boolean rotRight = false;
-    private boolean rotLeft = false;
-    private boolean fwd = false;
-    private boolean back = false;
-    private boolean left = false;
-    private boolean right = false;
-    private boolean exit;
+	private double speedm;
+	private boolean rotRight = false;
+	private boolean rotLeft = false;
+	private boolean fwd = false;
+	private boolean back = false;
+	private boolean left = false;
+	private boolean right = false;
+	private boolean exit;
 
-    public KeyHandler(Controller c, Returner g) {
-        con = c;
-        game = g;
-        s = con.getSpieler();
-        speedm = s.getSpeed();
+	public KeyHandler(Controller c, Returner g) {
+		con = c;
+		game = g;
+		s = con.getSpieler();
+		speedm = s.getSpeed();
 
-    }
+	}
 
-    public void mouseClicked(MouseEvent e) {
+	public void movesPlayer() {
+		if (fwd == true) {
+			s.geradeGehen();
+		}
+		if (back == true) {
+			s.rueckwaertsGehen();
+		}
+		if (left == true) {
+			s.linksGehen();
+		}
+		if (right == true) {
+			s.rechtsGehen();
+		}
+		if (rotRight == true) {
+			s.rechtsdrehen();
+		}
+		if (rotLeft == true) {
+			s.linksdrehen();
+		}
+		if (exit == true) {
+			System.exit(0);
+		}
+	}
 
-        // stat.playerSchiesen();
+	public void movemPlayer() {
+		if (fwd == true) {
+			s.geradeGehen();
+		}
+		if (back == true) {
+			s.rueckwaertsGehen();
+		}
+		if (left == true) {
+			s.linksGehen();
+		}
+		if (right == true) {
+			s.rechtsGehen();
+		}
+		if (rotRight == true) {
+			s.rechtsdrehen();
+		}
+		if (rotLeft == true) {
+			s.linksdrehen();
+		}
+		double x9 = s.getX();
+		double y9 = s.getY();
+		double r9 = s.getRotation();
+		String sql = "UPDATE multiplayer SET xposition = " + x9 + ", yposition = " + y9 + ", rotation = " + r9
+				+ " WHERE name = '" + s.getUsername() + "'";
+		datenbankupdaten(sql);
+		if (exit == true) {
+			datenbankupdaten("DELETE FROM multiplayer WHERE name = '" + s.getUsername() + "'");
+			System.exit(0);
+		}
+	}
 
-        game.dealDamage();
-    }
+	private void settings() {
+		game.getFrame().setVisible(false);
+		con.getCast().setRun(false);
+		new Settings(game, s.getSpeed(), s.getSpeedr(), con);
+	}
 
-    public void movesPlayer() {
-        if (fwd == true) {
-            s.geradeGehen();
-        }
-        if (back == true) {
-            s.rueckwaertsGehen();
-        }
-        if (left == true) {
-            s.linksGehen();
-        }
-        if (right == true) {
-            s.rechtsGehen();
-        }
-        if (rotRight == true) {
-            s.rechtsdrehen();
-        }
-        if (rotLeft == true) {
-            s.linksdrehen();
-        }
-        if (exit == true) {
-            System.exit(0);
-        }
-    }
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			fwd = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			back = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			left = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			right = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_F) {
+			settings();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			exit = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			rotRight = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			rotLeft = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			s.setSpeed(speedm * 2);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			game.dealDamage();
+		}
+	}
 
-    public void movemPlayer() {
-        if (fwd == true) {
-            s.geradeGehen();
-        }
-        if (back == true) {
-            s.rueckwaertsGehen();
-        }
-        if (left == true) {
-            s.linksGehen();
-        }
-        if (right == true) {
-            s.rechtsGehen();
-        }
-        if (rotRight == true) {
-            s.rechtsdrehen();
-        }
-        if (rotLeft == true) {
-            s.linksdrehen();
-        }
-        double x9 = s.getX();
-        double y9 = s.getY();
-        double r9 = s.getRotation();
-        String sql = "UPDATE multiplayer SET xposition = " + x9 + ", yposition = " + y9 + ", rotation = " + r9
-                + " WHERE name = '" + s.getUsername() + "'";
-        datenbankupdaten(sql);
-        if (exit == true) {
-            datenbankupdaten("DELETE FROM multiplayer WHERE name = '" + s.getUsername() + "'");
-            System.exit(0);
-        }
-    }
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			fwd = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			back = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			left = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			right = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			exit = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			rotRight = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			rotLeft = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			s.setSpeed(speedm);
+		}
+	}
 
-    private void settings() {
-        game.getFrame().setVisible(false);
-        con.getCast().setRun(false);
-        new Settings(game, s.getSpeed(), s.getSpeedr(), con);
-    }
+	public void datenbankupdaten(String sql) {
+		Connection verbindung = null;
+		verbindung = aufbau(verbindung);
+		try {
+			Statement st = verbindung.createStatement();
+			st.executeUpdate(sql);
+			st.close();
+		} catch (SQLException e) {
+			System.err.println("Fehler beim Einfuegen des Datensatzes: " + e);
+			System.exit(0);
+		}
+		abbau(verbindung);
+	}
 
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_W) {
-            fwd = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_S) {
-            back = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_A) {
-            left = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_D) {
-            right = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_F) {
-            settings();
-        }
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-            exit = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            rotRight = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            rotLeft = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-            s.setSpeed(speedm * 2);
-        }
-    }
+	private Connection aufbau(Connection ver) {
+		try {
+			ver = DriverManager.getConnection("jdbc:mysql://srvxampp/q11wolfenstein", "q11wolfenstein", "abitur");
+			return ver;
+		} catch (Exception e) {
+			System.err.println("Datenbankfehler(Verbindungsaufbau): " + e);
+			System.exit(0);
+			return null;
+		}
+	}
 
-    public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_W) {
-            fwd = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_S) {
-            back = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_A) {
-            left = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_D) {
-            right = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-            exit = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            rotRight = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            rotLeft = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-            s.setSpeed(speedm);
-        }
-    }
-
-    public void keyTyped(KeyEvent e) {
-    }
-
-    public void datenbankupdaten(String sql) {
-        Connection verbindung = null;
-        verbindung = aufbau(verbindung);
-        try {
-            Statement st = verbindung.createStatement();
-            st.executeUpdate(sql);
-            st.close();
-        } catch (SQLException e) {
-            System.err.println("Fehler beim Einfuegen des Datensatzes: " + e);
-            System.exit(0);
-        }
-        abbau(verbindung);
-    }
-
-    private Connection aufbau(Connection ver) {
-        try {
-            ver = DriverManager.getConnection("jdbc:mysql://srvxampp/q11wolfenstein", "q11wolfenstein", "abitur");
-            return ver;
-        } catch (Exception e) {
-            System.err.println("Datenbankfehler(Verbindungsaufbau): " + e);
-            System.exit(0);
-            return null;
-        }
-    }
-
-    private void abbau(Connection ver) {
-        try {
-            ver.close();
-        } catch (SQLException e) {
-            System.err.println("Fehler beim schliesen der Verbindung:" + e);
-            System.exit(0);
-        }
-    }
+	private void abbau(Connection ver) {
+		try {
+			ver.close();
+		} catch (SQLException e) {
+			System.err.println("Fehler beim schliesen der Verbindung:" + e);
+			System.exit(0);
+		}
+	}
 }
